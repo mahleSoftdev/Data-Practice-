29f0fc957480547605ded22c9ec55a509515cc3f
Asemahle Myeni <Asemahlemyeni10@gmail.com>
2026-09-22 22:21:16 +0200

    Creation of procedure

--- StudentNumber_DATA6222_Exam_Question 1.sql
+++ StudentNumber_DATA6222_Exam_Question 1.sql
@@ -15,3 +15,26 @@ FROM TUTOR_BOOKINGS
 WHERE BOOKING_DATE >= '2024-01-01' 
   AND BOOKING_DATE <= '2024-12-31'
   AND BookingDuration >= 100;
+
+--1.2) Create a stored procedure named ‘FindBookings’ that will display the
+--TUTOR_NAME, STUDENT_NAME, STUDENT_CELL, BOOKING_DATE,
+--BOOKING_TIME, AND BOOKING_DURATION of all sessions booked for a specific
+--tutor. When executing the stored procedure make use of the TUTOR_ID ‘ID0002’.
+CREATE PROCEDURE FindBookings
+    @TutorID INT
+AS
+BEGIN
+    SELECT 
+        t.TutorName       AS TUTOR_NAME,
+        s.StudentName     AS STUDENT_NAME,
+        s.StudentCell     AS STUDENT_CELL,
+        tb.BOOKING_DATE,
+        tb.BookingTime    AS BOOKING_TIME,
+        tb.BookingDuration AS BOOKING_DURATION
+    FROM TUTOR_BOOKINGS tb
+    INNER JOIN Student s ON tb.STUDENT_ID = s.STUDENT_ID
+    INNER JOIN TUTOR   t ON tb.TUTOR_ID   = t.TUTOR_ID
+    WHERE tb.TUTOR_ID = @TutorID;
+END;
+
+
